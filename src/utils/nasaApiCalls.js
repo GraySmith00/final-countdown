61623ac9bf5687f79eb5dfe1895868972c816dc9
Gray Smith <gray.smith00@gmail.com>
2018-11-06 15:47:35 -0700

    Add date param to nasaApiCall

--- src/utils/nasaApiCalls.js
+++ src/utils/nasaApiCalls.js
@@ -1,8 +1,8 @@
-export const nasaApiCall = async () => {
+export const nasaApiCall = async date => {
   const response = await fetch(
     `https://api.nasa.gov/planetary/apod?api_key=${
       process.env.VUE_APP_NASA_KEY
-    }`
+    }&date=${date}`
   );
   let data = await response.json();
   return data;
