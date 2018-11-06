fec4a42945f32f2420bd8ff6dcfdff774f7f498d
Gray Smith <gray.smith00@gmail.com>
2018-11-06 15:43:50 -0700

    Remove margin from App.vue

--- src/App.vue
+++ src/App.vue
@@ -17,11 +17,24 @@ export default {
 
 <style>
 #app {
+  margin: 0 auto;
+  padding: 2rem;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+
+  background: #4b6cb7; /* fallback for old browsers */
+  background: -webkit-linear-gradient(
+    to right,
+    #182848,
+    #4b6cb7
+  ); /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(
+    to right,
+    #182848,
+    #4b6cb7
+  ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 }
 </style>
