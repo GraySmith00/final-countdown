3b6057abbf7d0fe0b2baf422e012a6f0579c86ff
Gray Smith <gray.smith00@gmail.com>
2018-11-06 15:47:56 -0700

    Add conditional rendering of today or yesterday and styling

--- src/components/NasaInfo.vue
+++ src/components/NasaInfo.vue
@@ -1,14 +1,24 @@
 <template>
   <div class="nasa-info">
-    <h1>NASA Daily</h1>
-    <div class="nasa-info">
-      <img :src="nasaInfo.url" alt="nasa" width="200px"/>
-      <p class="date">{{ nasaInfo.date }}</p>
-      <p class="explanation">{{ nasaInfo.explanation }}</p>
-      <p class="copyright">{{nasaInfo.copyright}}</p>
-      
+    <div class="nasa-info-inner">
+      <h1>NASA Daily</h1>
+      <div class="nasa-info">
+        <div v-if="!displayYesterday" class="today">
+          <img :src="todayInfo.url" alt="nasa" width="200px"/>
+          <h3 class="date">{{ todayInfo.date }}</h3>
+          <p class="explanation">{{ todayInfo.explanation }}</p>
+          <p class="copyright"><i class="far fa-copyright"></i> {{todayInfo.copyright}}</p>        
+        </div>
+        <div v-if="displayYesterday" class="yesterday">
+          <img :src="yesterdayInfo.url" alt="nasa" width="200px"/>
+          <h3 class="date">{{ yesterdayInfo.date }}</h3>
+          <p class="explanation">{{ yesterdayInfo.explanation }}</p>
+          <p class="copyright"><i class="far fa-copyright"></i> {{yesterdayInfo.copyright}}</p>
+        </div>  
+      </div>
+      <button @click="displayYesterday = false">Today</button>
+      <button @click="displayYesterday = true">Yesterday</button>
     </div>
-
   </div>
 </template>
 
@@ -19,18 +29,40 @@ export default {
   name: 'NasaInfo',
   data() {
     return {
-      nasaInfo: {}
+      todayInfo: {},
+      yesterdayInfo: {},
+      displayYesterday: true
     };
   },
   async created() {
-    let data = await nasaApiCall();
-    this.nasaInfo = data;
+    let today = new Date().toISOString().slice(0, 10);
+    let todayData = await nasaApiCall(today);
+    this.todayInfo = todayData;
+
+    let yesterday = new Date(Date.now() - 864e5).toISOString().slice(0, 10);
+    let yesterdayData = await nasaApiCall(yesterday);
+    this.yesterdayInfo = yesterdayData;
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.nasa-info {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.nasa-info-inner {
+  width: 50%;
+  background: rgba(0, 0, 0, 0.5);
+  color: #fff;
+  padding: 2rem;
+  border-radius: 10px;
+}
+.explanation {
+  text-align: justify;
+}
 h3 {
   margin: 40px 0 0;
 }
