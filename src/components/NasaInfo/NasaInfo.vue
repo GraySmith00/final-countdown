7ddf45dafafa0e705ebbd9028ed173b13c9a8a85
Gray Smith <gray.smith00@gmail.com>
2018-11-06 19:47:22 -0700

    Work on NasaInfo styling

--- src/components/NasaInfo/NasaInfo.vue
+++ src/components/NasaInfo/NasaInfo.vue
@@ -3,21 +3,40 @@
     <div class="nasa-info-inner">
       <h1>NASA Daily</h1>
       <div class="nasa-info">
-        <div v-if="!displayYesterday" class="today">
-          <img :src="todayInfo.url" alt="nasa" width="200px"/>
-          <h3 class="date">{{ formattedDate.today }}</h3>
-          <p class="explanation">{{ todayInfo.explanation }}</p>
-          <p class="copyright"><i class="far fa-copyright"></i> {{todayInfo.copyright}}</p>        
+        <div v-if="!displayYesterday" class="info-container today">
+          <div class="image-container">
+            <img :src="todayInfo.url" alt="nasa" width="200px"/>
+            <h3 class="date">{{ formattedDate.today }}</h3>
+            <p class="copyright"><i class="far fa-copyright"></i> 
+              {{todayInfo.copyright}}
+            </p>        
+          </div>
+          <div class="explanation-buttons">
+            <p class="explanation">{{ todayInfo.explanation }}</p>
+            <div class="buttons">
+              <button @click="displayYesterday = false">Today</button>
+              <button @click="displayYesterday = true">Yesterday</button>
+            </div>
+          </div>
         </div>
-        <div v-if="displayYesterday" class="yesterday">
-          <img :src="yesterdayInfo.url" alt="nasa" width="200px"/>
-          <h3 class="date">{{ formattedDate.yesterday }}</h3>
-          <p class="explanation">{{ yesterdayInfo.explanation }}</p>
-          <p class="copyright"><i class="far fa-copyright"></i> {{yesterdayInfo.copyright}}</p>
+        <div v-if="displayYesterday" class="info-container yesterday">
+          <div class="image-container">
+            <img :src="yesterdayInfo.url" alt="nasa" width="200px"/>
+            <h3 class="date">{{ formattedDate.yesterday }}</h3>
+            <p class="copyright"><i class="far fa-copyright"></i> 
+              {{yesterdayInfo.copyright}}
+            </p>
+          </div>
+          <div class="explanation-buttons">
+            <p class="explanation">{{ yesterdayInfo.explanation }}</p>
+            <div class="buttons">
+              <button @click="displayYesterday = false">Today</button>
+              <button @click="displayYesterday = true">Yesterday</button>
+            </div>
+          </div>
         </div>  
       </div>
-      <button @click="displayYesterday = false">Today</button>
-      <button @click="displayYesterday = true">Yesterday</button>
+      
     </div>
   </div>
 </template>
@@ -67,14 +86,29 @@ export default {
 }
 .nasa-info-inner {
   width: 50%;
+  height: 500px;
   background: rgba(0, 0, 0, 0.5);
   color: #fff;
   padding: 2rem;
   border-radius: 10px;
   box-shadow: 10px 10px 43px -12px rgba(0, 0, 0, 0.75);
 }
+.info-container {
+  display: grid;
+  grid-template-columns: 5fr 10fr;
+  grid-gap: 1rem;
+}
+.date {
+  margin-top: 1rem;
+}
 .explanation {
+  margin-top: 0;
+  height: 300px;
   text-align: justify;
+  overflow: scroll;
+}
+.buttons {
+  margin-top: 2rem;
 }
 h3 {
   margin: 40px 0 0;
